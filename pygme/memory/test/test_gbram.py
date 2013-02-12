711be88b3bb89f08be4ea61339d3245cd938784b
Sean Kelleher <ezanmoto@gmail.com>
2013-02-12 01:25:53 +0000

    Added test for persistence of memory

--- pygme/memory/test/test_gbram.py
+++ pygme/memory/test/test_gbram.py
@@ -59,6 +59,18 @@ class TestGBRAM(unittest.TestCase):
         with self.assertRaises(ValueError):
             self._mem.set8(0x8000, 0x100)
 
+    def test_get8_withSetMemoryAddress_getsCorrectData(self):
+        for addr in [0x8000, 0xffff]:
+            self._expect8(addr, 0x00)
+            for val in [0x00, 0xff]:
+                self._mem.set8(addr, val)
+                self._expect8(addr, val)
+
+    def _expect8(self, addr, v):
+        b = self._mem.get8(addr)
+        self.assertEquals(b, v, "Expected 0x%02x(%d), got 0x%02x(%d) at 0x%x"
+            % (v, v, b, b, addr))
+
     def tearDown(self):
         self._mem = None
 
