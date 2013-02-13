88f36172fa42faa36b5e04b409928a2cf14d50e2
Sean Kelleher <ezanmoto@gmail.com>
2013-02-13 01:36:43 +0000

    Added to mode set test

--- pygme/cartridge/test/test_cart.py
+++ pygme/cartridge/test/test_cart.py
@@ -40,9 +40,13 @@ class TestCartridge(unittest.TestCase):
             cartridge.set8(addr, 1)
             self.assertEquals(cartridge.getMode(), 0)
         for addr in [0x6000, 0x7fff]:
-            cartridge.set8(addr, 1)
+            cartridge.set8(addr, 0b11)
             self.assertEquals(cartridge.getMode(), 1)
-            cartridge.set8(addr, 0)
+            cartridge.set8(addr, 0b10)
+            self.assertEquals(cartridge.getMode(), 0)
+            cartridge.set8(addr, 0b01)
+            self.assertEquals(cartridge.getMode(), 1)
+            cartridge.set8(addr, 0b00)
             self.assertEquals(cartridge.getMode(), 0)
 
     def test_set8_ofROMFourthQuarter_raisesRuntimeError(self):
