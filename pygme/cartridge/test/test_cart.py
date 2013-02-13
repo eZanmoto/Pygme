18c33896a9ada15997d35df9a65ce692b8a8e60e
Sean Kelleher <ezanmoto@gmail.com>
2013-02-13 02:09:01 +0000

    Updated to use global constants defined in cart module

--- pygme/cartridge/test/test_cart.py
+++ pygme/cartridge/test/test_cart.py
@@ -6,33 +6,30 @@ import unittest
 
 from pygme.cartridge import cart
 
+MIN_ADDR = 0x0000
+HEADER_END_ADDR = 0x0150
+
+CART_TYPE_MBC1 = 1
+
 class TestCartridge(unittest.TestCase):
     """Tests for Cartridge class."""
-
-    MIN_ADDR = 0x0000
-    HEADER_END_ADDR = 0x0150
-    MAX_ADDR = 0x7fff
-
-    CART_TYPE_ADDR = 0x0147
-
-    CART_TYPE_MBC1 = 1
 
     def test_set8_ofNegativeMBC1Address_raisesIndexError(self):
         cartridge = cart.Cartridge(self._newMBC1())
         with self.assertRaises(IndexError):
-            cartridge.set8(self.MIN_ADDR - 1, 0)
+            cartridge.set8(MIN_ADDR - 1, 0)
 
     def _newMBC1(self):
         rom = []
         for i in range(0, 4):
-            rom.extend([i for _ in range(self.MIN_ADDR, self.MAX_ADDR)])
-        rom[self.CART_TYPE_ADDR] = self.CART_TYPE_MBC1
+            rom.extend([i for _ in range(MIN_ADDR, cart.MAX_ADDR)])
+        rom[cart.CART_TYPE_ADDR] = CART_TYPE_MBC1
         return rom
 
     def test_set8_withOverflowAddress_raisesIndexError(self):
         cartridge = cart.Cartridge(self._newMBC1())
         with self.assertRaises(IndexError):
-            cartridge.set8(self.MAX_ADDR + 1, 0)
+            cartridge.set8(cart.MAX_ADDR + 1, 0)
 
     def test_set8_ofMBC1FourthQuarter_setsMode(self):
         cartridge = cart.Cartridge(self._newMBC1())
@@ -56,7 +53,7 @@ class TestCartridge(unittest.TestCase):
             cartridge.set8(0x6000, 0)
 
     def _newROM(self):
-        return [0 for _ in range(self.MIN_ADDR, self.MAX_ADDR + 1)]
+        return [0 for _ in range(MIN_ADDR, cart.MAX_ADDR + 1)]
 
     def test_set8_ofMBC1SecondQuarter_setsBank(self):
         cartridge = cart.Cartridge(self._newMBC1())
@@ -83,7 +80,7 @@ class TestCartridge(unittest.TestCase):
 
     def test_get8_ofMBC1FirstHalf_doesntChangeWithBanking(self):
         cartridge = cart.Cartridge(self._newMBC1())
-        for addr in range(self.HEADER_END_ADDR, (self.MAX_ADDR + 1) / 2):
+        for addr in range(HEADER_END_ADDR, (cart.MAX_ADDR + 1) / 2):
             val = cartridge.get8(addr)
             self.assertEquals(val, 0, "ROM[0x%04x] = 0x%02x, expected 0x%02x" %
                     (addr, val, 0))
