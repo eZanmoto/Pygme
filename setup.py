bab31123b307f449711aff1a341ca204304d83b2
Sean Kelleher <ezanmoto@gmail.com>
2013-02-23 14:31:41 +0000

    Fixed list style according to new style rule

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@
 
 from setuptools import setup
 
-setup(name='Pygme',
-      version="0.1",
-      description="Python Gameboy Emuator",
-      classifiers=[
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python",
-      ],
-      author="Sean Kelleher",
-      author_email='ezanmoto@gmail.com',
-      license="GPL",
-      packages=['pygme', 'pygme.cpu', 'pygme.cpu.test', ],
-      )
+setup(
+    name='Pygme',
+    version="0.1",
+    description="Python Gameboy Emuator",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python",
+    ],
+    author="Sean Kelleher",
+    author_email='ezanmoto@gmail.com',
+    license="GPL",
+    packages=['pygme', 'pygme.cpu', 'pygme.cpu.test', ],
+)
