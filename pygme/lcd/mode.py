f0c61bfb13869a1c8c6c284f5feaf28a1140b8c7
Sean Kelleher <ezanmoto@gmail.com>
2014-06-22 09:56:07 +0200

    Added LCDMode

---
+++ pygme/lcd/mode.py
@@ -0,0 +1,9 @@
+# Copyright 2014 Sean Kelleher. All rights reserved.
+# Use of this source code is governed by a GPL
+# license that can be found in the LICENSE file.
+
+class LCDMode:
+    HBLANK = 0
+    VBLANK = 1
+    OAM_READ = 2
+    VRAM_READ = 3
